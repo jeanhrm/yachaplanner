90581f77609b378f504e3d3215c8f567c8e85764
jeanhrm <ingempjordonez@gmail.com>
2026-04-21 20:38:01 -0500

    fix: add debug output to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+echo "=== Running migrations ==="
 php artisan migrate --force
+echo "=== Running seeders ==="
 php artisan db:seed --force
+echo "=== Starting server ==="
 php artisan serve --host=0.0.0.0 --port=8080
